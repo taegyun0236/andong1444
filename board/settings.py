73b129ff65309a5a06f7afb1625b087afc2fce55
taegyun <taegyun9307@naver.com>
2020-08-11 16:43:37 +0900

    ALLOWED_HOSTS

--- board/settings.py
+++ board/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'in^v!(lt2dqnlz5)_f2fn1*m8^_y$8@j442(x$j(b3wyy%zpkt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.pythonanywhere.com','localhost','127.0.0.1']
 
 
 # Application definition
